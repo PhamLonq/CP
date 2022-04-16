c1c6352738f183e93a7b6c3d1195f102a15a7c6b
unknown <corncake12233@gmail.com>
2022-04-16 11:29:23 +0700

    update .jason

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'object',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '3aeaf1df9d0119d9648f3ba568c966ec5fb1fb0b',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'object',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '3aeaf1df9d0119d9648f3ba568c966ec5fb1fb0b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
